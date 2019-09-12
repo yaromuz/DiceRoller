d26b500a47c50972b427d700fcf45f6b90970a53
Tanya Kontsevich <tanya@Tanyas-MacBook-Pro.local>
2019-09-12 15:21:08 -0700

    change dice text to images

--- app/src/main/java/com/example/tkontsevich/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/tkontsevich/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tkontsevich.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
@@ -19,8 +20,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random().nextInt(6)+1
-        resultText.text= randomInt.toString()
+        val drawableResourse = when(randomInt){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
     }
 }
